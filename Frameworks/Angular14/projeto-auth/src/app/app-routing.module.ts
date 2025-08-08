68fc067852aee826083c449105f2fe350f93ba38
ju130-star <julyaestrela41@gmail.com>
2025-08-08 08:21:18 -0300

    08/08/25 Parte Final

--- Frameworks/Angular14/projeto-auth/src/app/app-routing.module.ts
+++ Frameworks/Angular14/projeto-auth/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AuthGuard } from './guards/auth.guard';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { InternaComponent } from './pages/interna/interna.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'registro', component: RegistroComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'interna', component: InternaComponent, canActivate: [AuthGuard] },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frameworks/Angular14/projeto-auth/src/app/services/auth.service.ts
+++ Frameworks/Angular14/projeto-auth/src/app/services/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { catchError, map, Observable, switchMap, tap, throwError } from 'rxjs';
+
 @Injectable({
   providedIn: 'root',
 })
@@ -12,30 +13,53 @@ export class AuthService {
 
   constructor(private router: Router, private http: HttpClient) {}
 
- strar(usuario:any):Observable<any>{
+registrar(usuario:any):Observable<any>{
+  //primeiro verifica se o usuario já existe
    return this.http.get<any[]>(`${this.apiUrl}?email=${usuario.email}`).pipe(
      switchMap((usuarios) => {
-       if (usuarios.length > 0) {
+       if (usuarios.length > 0) { //caso exista
+        //cria uma mensagem de erro
          return throwError(() => new Error('Usuário Já Cadastrado'));
+          }else{// caso não exista
+          //cadastra o usuario no BD
+          return this.http.post<any>(this.apiUrl, usuario);
        }
      })
    );
  }
  login(credenciais: any): Observable<boolean>{
+  //pega as credenciais do usuario (email e senha)
     return this.http.get<any[]>(
+      //verica no BD se email e senha foram encontrados
       `${this.apiUrl}?email=${credenciais.email}&senha=${credenciais.senha}`).pipe(
         map(usuarios => {
-          if(usuarios.length>0){
+          if(usuarios.length>0){ //se encontrado
+            // armazena as informações do usuário e a chave no localStorage
             localStorage.setItem(this.CHAVE_AUTH, JSON.stringify(usuarios[0]));//converte em texto
+              //retorna que o acesso foi permitido
             return true;
-          }else{
+          }else{ //caso encontrado
             //fazer um erro
+            //retorno que meu usuario não esta permitido o acesso
             return false;
           }
         })
       )
+    }
+  logout(){
+    localStorage.removeItem(this.CHAVE_AUTH); //remove a chave de autenticação do usuario
+    this.router.navigate(['/home']);//redireciona para a a pagina inicial
+  }
 
-  logout(){}
+  //verifica se o usuario já está logado (CHAVE_AUTH)
+  estaAutenticado(): boolean {
+    //transforma uma variavel do tipo texto em boolean
+    return !!localStorage.getItem(this.CHAVE_AUTH) //vai retornar true ou false
+  }
+
+  //pega as informações do usuario no localStorage
+  getUsuarioAtual(): any{
+    //retorna as informações do usário autenticado , que estão armazenadas no localStorage
+    return JSON.parse(localStorage.getItem(this.CHAVE_AUTH) || '{}'); //pega o usuario logado
+  }
 }
-
-
